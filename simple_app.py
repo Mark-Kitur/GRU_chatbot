68a2780c0936fdaa40aaa96272e1287d979b0e12
Mark Kitur <kimutai.workspace@gmail.com.com>
2026-02-05 00:35:51 +0300

    add decoder

--- simple_app.py
+++ simple_app.py
@@ -27,39 +27,69 @@ class attn(nn.Module):
     def __init__(self, method, hidden_size):
         super().__init__()
         self.method = method
-        if self.method not in ['dot', 'general', 'concat']:
-            raise ValueError(self.method, "is not a valid attention method")
         self.hidden_size = hidden_size
+
+        if self.method not in ['dot', 'general','concat']:
+            raise ValueError(self.method, 'is not an appropriate attention method.')
         if self.method == 'general':
-            self.attn = nn.Linear(hidden_size, hidden_size)
+            self.attn = nn.Linear(self.hidden_size, hidden_size)
         elif self.method == 'concat':
-            self.attn = nn.Linear(hidden_size *2, hidden_size)
+            self.attn = nn.Linear(self.hidden_size *2, hidden_size)
             self.v = nn.Parameter(torch.FloatTensor(hidden_size))
 
     def dot_score(self, hidden, encoder_output):
         return torch.sum(hidden * encoder_output, dim=2)
-
     def general_score(self, hidden, encoder_output):
         energy = self.attn(encoder_output)
         return torch.sum(hidden * energy, dim=2)
-
     def concat_score(self, hidden, encoder_output):
         energy = self.attn(torch.cat((hidden.expand(encoder_output.size(0), -1, -1), encoder_output),2)).tanh()
         return torch.sum(self.v * energy, dim=2)
-
-    def forward(self, hidden, encoder_outputs):
-        if self.method == 'dot':
-            attn_energies = self.dot_score(hidden, encoder_outputs)
-        elif self.method == 'general':
-            attn_energies = self.general_score(hidden, encoder_outputs)
-        elif self.method == 'concat':
-            attn_energies = self.concat_score(hidden, encoder_outputs)
-
-        attn_energies = attn_energies.t()
-        return nn.functional.softmax(attn_energies, dim=1)
     
+    def forward(self, hidden, encoder_output):
+        if self.method == 'dot':
+            attn_energies = self.dot_score(hidden, encoder_output)
+        elif self.method == 'general':
+            attn_energies = self.general_score(hidden, encoder_output)
+        elif self.method == 'concat':
+            attn_energies = self.concat_score(hidden, encoder_output)
+        
+        attn_energies = attn_energies.t()
+        return nn.functional.softmax(attn_energies, dim=1).unsqueeze(1)
 
-# greedy search
+class Decoder(nn.Module):
+    def __init__(self, attn_model, embedding,hidden_size, output_size, n_layers, dropout=0.1):
+        super().__init__()
+        self.att_model = attn_model
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.n_layers = n_layers
+        self.dropout =dropout
+
+        self.embedding = embedding
+        self.embedding_dropout = nn.Dropout(dropout)
+        self.gru = nn.GRU(hidden_size,hidden_size, n_layers, dropout=(0 if n_layers ==1 else dropout))
+        self.concat = nn.Linear(hidden_size *2, hidden_size)
+        self.out = nn.Linear(hidden_size,output_size)
+
+        self.attn = attn(attn_model, hidden_size)
+
+    def forward(self, input_step, last_hidden, encoder_outputs):
+        embedded = self.embedding(input_step)
+        embedded = self.embedding_dropout(embedded)
+        rnn_output, hidden = self.gru(embedded, last_hidden)
+        attn_weights = self.attn(rnn_output, encoder_outputs)
+        context = attn_weights.bmm(encoder_outputs.transpose(0,1))
+        rnn_output = rnn_output.squeeze(0)
+        context = context.squeeze(1)
+        concat_input = torch.cat((rnn_output, context),1)
+        concat_output = torch.tanh(self.concat(concat_input))
+        output = self.out(concat_output)
+        output = nn.functional.softmax(output, dim=1)
+
+        return output, hidden
+
+
 class GreedySearch(nn.Module):
     def __init__(self, encoder, decoder):
         super().__init__()
